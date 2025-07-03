a02830e5a0f3a58f81200b6f9584f392d949eea3
413637462 <jason20050701@gmail.com>
2025-07-03 10:12:29 +0800

    add

---
+++ 0703/sum.java
@@ -0,0 +1,9 @@
+public class sum {
+    public static void main(String[] args) {
+        int n1 = 12;
+        int n2 = 26;
+        int sum = n1 + n2 ;
+        System.out.println(sum);
+    }
+    
+}
